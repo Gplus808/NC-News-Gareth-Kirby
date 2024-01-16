dd9f926127b08dc5b473b7bcb0fa710a7d5c21cf
Gplus808 <gplus808@gmail.com>
2024-01-16 12:51:46 +0000

    task 3 completed with dynamic testing (revised)

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const { getTopics } = require('./Controllers/topics.controller');
+const fs = require('fs');
 const app = express();
 
 app.get('/api/topics', getTopics)
 
+let endPointsFs;
+fs.readFile('endpoints.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error("Error reading endpoints.json", err);
+        return err;
+    }
+    endPointsFs = JSON.parse(data)
+    // console.log(endPointsFs)
+    return endPointsFs
+})
 
-module.exports = app;
+app.get('/api', (req, res) => {
+    if (!endPointsFs) {
+        res.status(500).send("Endpoint isn't available");
+        return
+    }
+    return res.json(endPointsFs)
+})
+
+
+module.exports = app,  endPointsFs;
--- app.test.js
+++ app.test.js
@@ -1,9 +1,10 @@
 const request = require("supertest");
 const db = require("./db/connection")
-
 const app = require("./app")
 const seed = require("./db/seeds/seed")
 const testData = require("./db/data/test-data")
+const fs = require('fs').promises;
+
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
@@ -14,7 +15,6 @@ describe('/api/topics', () => {
         .get('/api/topics')
         .expect(200)
         .then((response) => {
-            console.log(response.body.topics)
             expect(response.body.topics.length).toBe(3);
             response.body.topics.forEach((topic) => {
                 expect(typeof topic.description).toBe('string');
@@ -22,4 +22,23 @@ describe('/api/topics', () => {
             });
         });
     });
+});
+
+describe('/api/', () => {
+    test('GET:200 returns an object describing all the available endpoints on your API', () => {
+        return fs.readFile('endPoints.json', 'utf8')
+        .then((data) => {
+        const expected = JSON.parse(data)
+        return request(app)
+        .get('/api/')
+        .expect(200)
+        .then((response) => {
+         expect(response.body).toEqual(expected)
+        })
+      })
+      .catch(err => {
+        console.error(err);
+        return err;
+  });
+});
 });
