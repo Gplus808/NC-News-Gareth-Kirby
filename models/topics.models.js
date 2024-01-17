afeeb76137091328ea41107d8e773adfd7f06531
Gplus808 <gplus808@gmail.com>
2024-01-17 14:13:12 +0000

    task 4 completed with error tests

--- models/topics.models.js
+++ models/topics.models.js
@@ -6,3 +6,17 @@ exports.fetchTopics = () => {
         return result.rows
     })
 }
+
+exports.selectArticle = (articleId) => {
+    return db.query(`
+    SELECT *
+    FROM articles
+    WHERE articles.article_id = $1`, [articleId])
+    .then((response) => {
+        if (response.rows.length === 0) {
+            return Promise.reject({ msg: "Article not found"})
+        } else {
+        return response.rows[0]
+        }
+    })
+}
