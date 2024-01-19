99721cae7a84973e4bd8c35e8fa7e812b4f50c3a
Gplus808 <gplus808@gmail.com>
2024-01-19 15:21:07 +0000

    task9 complete

--- models/topics.models.js
+++ models/topics.models.js
@@ -82,13 +82,11 @@ exports.fetchAllArticles = () => {
     }
 
     exports.destroyComment = (comment_id) => {
-        console.log(comment_id)
         return db.query(`
         DELETE FROM comments
         WHERE comment_id = $1
         RETURNING *`, [comment_id])
         .then(({rows}) => {
-            console.log(rows[0], 'rows')
             if (rows[0] === undefined) {
               return Promise.reject({status: 404, msg : 'Comment not found'})
             }
