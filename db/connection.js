6717d66e3d2f15bc0071e188f5100113d899e145
Gplus808 <gplus808@gmail.com>
2024-02-05 12:08:44 +0000

    Hosting fixed

--- db/connection.js
+++ db/connection.js
@@ -1,25 +1,18 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
+const fs = require('fs');
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
-
 const config = {};
 
 if (ENV === 'production') {
-  // For production, use DATABASE_URL and set a max pool size
   config.connectionString = process.env.DATABASE_URL;
-  // Limiting the pool size to 2 to leave connections available for manual operations
   config.max = 2;
-} else {
-  // For non-production environments, no additional config is required here,
-  // assuming PGDATABASE and other necessary env vars are set.
-  // You might still configure other aspects like max pool size based on your development or test DB.
 }
-
-module.exports = new Pool();
+module.exports = new Pool(config);
